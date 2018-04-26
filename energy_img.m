0e0974334b0a6a4ac3f6a9df7f712c83dd32a587
Yash Pahade <yash.aries.6@gmail.com>
2018-04-26 13:24:23 -0700

    first commit

---
+++ energy_img.m
@@ -0,0 +1,9 @@
+function [energyImg] = energy_img(img)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+img = im2double(img);
+img = rgb2gray(img);
+[FX,FY] = gradient(img);
+energyImg = sqrt(FX^2 + FY^2);
+end
+
