86cf79ed622506b7c805a984c971ed31523714e0
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2018-04-29 01:06:07 -0700

    fixed syntax issue: view_seam is working

--- find_vertical_seam.m
+++ find_vertical_seam.m
@@ -9,11 +9,11 @@ verticalSeam(m) = min_idx;
 
 for x = m-1:-1:1
     if (min_idx == 1)
-        [~,min_idx] = min(cumulativeEnergyMap(x,min_idx), cumulativeEnergyMap(x, min_idx+1));
+        [~,min_idx] = min([cumulativeEnergyMap(x,min_idx), cumulativeEnergyMap(x, min_idx+1)]);
     elseif (min_idx == n)
-        [~,min_idx] = min(cumulativeEnergyMap(x,min_idx), cumulativeEnergyMap(x, min_idx-1));
+        [~,min_idx] = min([cumulativeEnergyMap(x,min_idx), cumulativeEnergyMap(x, min_idx-1)]);
     else
-        [~,min_idx] = min(cumulativeEnergyMap(x,min_idx-1), cumulativeEnergyMap(x, min_idx), cumulativeEnergyMap(x, min_idx+1));
+        [~,min_idx] = min([cumulativeEnergyMap(x,min_idx-1), cumulativeEnergyMap(x, min_idx), cumulativeEnergyMap(x, min_idx+1)]);
     end
     verticalSeam(x) = min_idx;
 end
