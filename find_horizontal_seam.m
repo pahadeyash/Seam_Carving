106bf9dcd258e5eeb1502b87ab8e1166c52c9be8
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2018-04-29 01:04:43 -0700

    merge with sai horizontal seam code

---
+++ find_horizontal_seam.m
@@ -0,0 +1,25 @@
+function horizontalSeam = find_horizontal_seam(cumulativeEnergyMap)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+[m,n] = size(cumulativeEnergyMap);
+
+horizontalSeam = zeros(1,n);
+
+% finding minimum in last column
+lastCol = cumulativeEnergyMap(:,n);
+min_val, min_idx = min(lastCol);
+horizontalSeam(n) = min_idx;
+
+% backtracking
+for y = n-1:-1:1
+    if (min_idx == m)
+        min_val, min_idx = min([cumulativeEnergyMap(min_idx-1, y), cumulativeEnergyMap(min_idx, y)]);
+    elseif (min_idx == 1)
+        min_val, min_idx = min([cumulativeEnergyMap(min_idx, y), cumulativeEnergy(min_idx+1, y)]);
+    else
+        min_val, min_idx = min([cumulativeEnergyMap(min_idx-1, y), cumulativeEnergyMap(min_idx, y), cumulativeEnergy(min_idx+1, y)]);
+    end
+    horizontalSeam(y) = min_idx;
+end
+
+end
