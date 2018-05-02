1c43b6fb21b8b27b86a417a61654171e4ebecad2
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2018-05-02 01:24:59 -0700

    removed unnecessary file

--- PS1_2_3.m
+++
@@ -1,6 +0,0 @@
-A = imread('inputSeamCarvingPrague.jpg');
-B = energy_img(A);
-C = cumulative_min_energy_map(B, 'VERTICAL');
-seam = find_vertical_seam(C);
-view_seam(A, seam, 'VERTICAL');
-
--- find_vertical_seam.m
+++ find_vertical_seam.m
@@ -1,15 +1,14 @@
 function [verticalSeam] = find_vertical_seam(cumulativeEnergyMap)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-    copy = cumulativeEnergyMap;
-    [m,n] = size(copy);
+    [m,n] = size(cumulativeEnergyMap);
     verticalSeam = zeros(1,m);
-    [~,temp_idx] = min(copy(m, :));
+    [~,temp_idx] = min(cumulativeEnergyMap(m, :));
     verticalSeam(m) = temp_idx;
     for i = m-1:-1:1
         index = verticalSeam(i+1);
         if verticalSeam(i+1) == 1
-            [~,temp_idx] = min([255, copy(i,temp_idx), copy(i, index+1)]);
+            [~,temp_idx] = min([inf, cumulativeEnergyMap(i,temp_idx), cumulativeEnergyMap(i, index+1)]);
             switch(temp_idx)
                 case 1
                     error('not possible: something is wrong');
@@ -19,17 +18,17 @@ function [verticalSeam] = find_vertical_seam(cumulativeEnergyMap)
                      verticalSeam(i) = index+1;
             end
         elseif verticalSeam(i+1) == n
-            [~,temp_idx] = min([copy(i,index-1), copy(i, index), 255]);
+            [~,temp_idx] = min([cumulativeEnergyMap(i,index-1), cumulativeEnergyMap(i, index), inf]);
             switch(temp_idx)
                 case 1
                     verticalSeam(i) = index-1;
                 case 2
                     verticalSeam(i) = index;
                 case 3
-                     error('This is not possible');
+                     error('not possible: somethis is wrong');
             end
         else
-            [~,temp_idx] = min([copy(i,index-1), copy(i,index), copy(i, index+1)]);
+            [~,temp_idx] = min([cumulativeEnergyMap(i,index-1), cumulativeEnergyMap(i,index), cumulativeEnergyMap(i, index+1)]);
             switch(temp_idx)
                 case 1
                     verticalSeam(i) = index-1;
