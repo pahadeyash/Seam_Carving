62921db2c8659aaadaba2b8466726c7546c2c386
Yash Pahade <yash.aries.6@gmail.com>
2018-05-01 19:41:49 -0700

    I think horizontal seam is working

--- find_horizontal_seam.m
+++ find_horizontal_seam.m
@@ -2,24 +2,33 @@ function horizontalSeam = find_horizontal_seam(cumulativeEnergyMap)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 [m,n] = size(cumulativeEnergyMap);
-
+ 
 horizontalSeam = zeros(1,n);
-
-% finding minimum in last column
-lastCol = cumulativeEnergyMap(:,n);
-[min_val, min_idx] = min(lastCol);
-horizontalSeam(n) = min_idx;
-
-% backtracking
-for y = n-1:-1:1
-    if (min_idx == m)
-        [min_val, min_idx] = min([cumulativeEnergyMap(min_idx-1, y), cumulativeEnergyMap(min_idx, y)]);
-    elseif (min_idx == 1)
-        [min_val, min_idx] = min([cumulativeEnergyMap(min_idx, y), cumulativeEnergyMap(min_idx+1, y)]);
-    else
-        [min_val, min_idx] = min([cumulativeEnergyMap(min_idx-1, y), cumulativeEnergyMap(min_idx, y), cumulativeEnergyMap(min_idx+1, y)]);
-    end
-    horizontalSeam(y) = min_idx;
-end
+% 
+% % finding minimum in last column
+% lastCol = cumulativeEnergyMap(:,n);
+% [~, min_idx] = min(lastCol);
+% horizontalSeam(n) = min_idx;
+% 
+% % backtracking
+% for y = n-1:-1:1
+%     if (min_idx == m)
+%         [~, temp_idx] = min([cumulativeEnergyMap(min_idx-1, y), cumulativeEnergyMap(min_idx, y)]);
+%     elseif (min_idx == 1)
+%         [~, temp_idx] = min([cumulativeEnergyMap(min_idx, y), cumulativeEnergyMap(min_idx+1, y)]);
+%     else
+%         [~, temp_idx] = min([cumulativeEnergyMap(min_idx-1, y), cumulativeEnergyMap(min_idx, y), cumulativeEnergyMap(min_idx+1, y)]);
+%     end
+%     
+%     if temp_idx == 2
+%         min_idx = min_idx - 1;
+%     elseif temp_idx ==3
+%         min_idx = min_idx + 1;
+%     end
+%     
+%     horizontalSeam(y) = min_idx; 
+% end
+A = transpose(cumulativeEnergyMap);
+horizontalSeam = find_vertical_seam(A);
 
 end
